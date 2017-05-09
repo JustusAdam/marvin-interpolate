3aa45c53c85d44ac7971a314632fe91a25d216a0
Justus Adam <dev@justus.science>
2017-05-09 15:15:33 +0200

    Added explicit type signatures to tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -29,47 +29,47 @@ main :: IO ()
 main = hspec $ do
     describe "parsing to itself" $ do
         it "#" $
-            [iq|#|] `shouldBe` "#"
+            [iq|#|] `shouldBe` ("#" :: String)
         it "#anything" $
-            [iq|#anything|] `shouldBe` "#anything"
+            [iq|#anything|] `shouldBe` ("#anything" :: String)
     
     describe "general parsing" $ do
         it "allows primes in at the end of binding names" $
-            let x' = "str" in [iq|Hello #{x'}|] `shouldBe` "Hello str"
+            let x' = "str" in [iq|Hello #{x'}|] `shouldBe` ("Hello str" :: String)
         it "allows primes in the middle of binding names" $
             let isn't = 5 :: Int in $(isS "Hello #{isn't}") `shouldBe` "Hello 5"
         
 
     describe "parsing escape sequences" $
         it "parses ## as #" $
-            [iq|##|] `shouldBe` "#"
+            [iq|##|] `shouldBe` ("#" :: String)
 
     describe "interpolation substitution" $ do
         it "leaves an empty string" $
-            [iq||] `shouldBe` ""
+            [iq||] `shouldBe` ("" :: String)
         it "returns a string unchanged if no variable is in it" $
-            [iq|this is not changed|] `shouldBe` "this is not changed"
+            [iq|this is not changed|] `shouldBe` ("this is not changed" :: String)
         it "interpolates just an external variable" $
-            let y = "str" in
+            let y = "str" :: String in
                 [iq|#{y}|] `shouldBe` y
         it "interpolates an external variable" $
-            let y = "str2" in [iq| hello you #{y} end|] `shouldBe` " hello you " ++ y ++ " end"
+            let y = "str2" :: String in [iq| hello you #{y} end|] `shouldBe` " hello you " ++ y ++ " end"
         it "interpolates the variable x (used to be special)" $
-            let x = "str" in [iq| hello x: #{x}|] `shouldBe` " hello x: " ++ x
+            let x = "str" :: String in [iq| hello x: #{x}|] `shouldBe` " hello x: " ++ x
         it "interpolates infix with $" $
             [iq|str #{show $ 4 + (5 :: Int)} str|] `shouldBe` "str 9 str"
         it "interpolates multiple bindings" $
             let
                 x = "multiple"
                 y = "can"
-                z = "local scope"
+                z = "local scope" :: String
             in [iq|We #{y} interpolate #{x} bindings from #{z}|]
                 `shouldBe` "We can interpolate multiple bindings from local scope"
 
         it "interpolates complex expressions" $
             let
                 x = ["haskell", "expression"]
-                y = " can be"
+                y = " can be" :: String
             in [iq|Any #{intercalate " " x ++ y} interpolated|]
                  `shouldBe` "Any haskell expression can be interpolated"
 
